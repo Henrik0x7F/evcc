ba76a2052a67806da3d781c38f9b3cad1ff97918
Henrik <henrik-public@gmx.net>
2024-05-25 13:25:30 +0200

    Remove imports

--- util/sponsor/auth.go
+++ util/sponsor/auth.go
@@ -1,14 +1,7 @@
 package sponsor
 
 import (
-	"context"
-	"fmt"
 	"time"
-
-	"github.com/evcc-io/evcc/api/proto/pb"
-	"github.com/evcc-io/evcc/util/cloud"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 var (
